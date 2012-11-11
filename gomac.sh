eeba6a701064d45566439f8f7640632ad86a784a
mastintin@gmail.com <mastintin@gmail.com@b5e14407-53c1-d272-d7d5-3beee06475fb>
2012-11-11 13:39:53 +0000

    added gomac.sh file
    git-svn-id: https://fiveweb.googlecode.com/svn/trunk@63 b5e14407-53c1-d272-d7d5-3beee06475fb

---
+++ gomac.sh
@@ -0,0 +1,10 @@
+# ./build.sh - (c) FiveTech Software 2007-2012
+
+clear
+
+echo compiling...
+BINPATH=/Applications/Xcode.app/Contents/Developer/usr/bin
+
+$BINPATH/make -f makefile_mac
+
+echo done!
