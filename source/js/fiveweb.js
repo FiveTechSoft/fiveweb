7423b9dfd36afb3989453462c1948865ce66f87a
antonio.fivetech@gmail.com <antonio.fivetech@gmail.com@b5e14407-53c1-d272-d7d5-3beee06475fb>
2012-11-14 08:49:15 +0000

    * More on previous change
    git-svn-id: https://fiveweb.googlecode.com/svn/trunk@90 b5e14407-53c1-d272-d7d5-3beee06475fb

--- source/js/fiveweb.js
+++ source/js/fiveweb.js
@@ -9,12 +9,18 @@ function SetTheme( cTheme )
 {
 	 var css = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/themes/name/jquery-ui.css"
 	
-   $( "link" ).eq( 0 ).attr( { href: css.replace( "name", cTheme ) } ); 
+   $( "link" ).eq( 0 ).attr( { href: css.replace( "name", cTheme ) } );
+   
+   if( $('link').eq( 1 ).length() > 0 ) 
+      $( "link" ).eq( 1 ).remove(); 
 }
 
 function SetAristoTheme()
 {
    $( "link" ).eq( 0 ).attr( { href: "http://taitems.github.com/Aristo-jQuery-UI-Theme/css/Aristo/Aristo.css" } ); 
+
+   if( $('link').eq( 1 ).length() > 0 ) 
+      $( "link" ).eq( 1 ).remove(); 
 }
 	
 function SetBootstrapTheme()
