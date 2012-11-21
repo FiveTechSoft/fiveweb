8cf99b40601cf8980e1e2de849b00baa7014fdf0
antonio.fivetech@gmail.com <antonio.fivetech@gmail.com@b5e14407-53c1-d272-d7d5-3beee06475fb>
2012-11-21 21:29:29 +0000

    * Fix to previous commit
    git-svn-id: https://fiveweb.googlecode.com/svn/trunk@150 b5e14407-53c1-d272-d7d5-3beee06475fb

--- source/js/fiveweb.js
+++ source/js/fiveweb.js
@@ -53,7 +53,7 @@ function AddButton( cDlgName )
    
    // oDlg.innerHTML += "<div id='" + cVarName + "' class='ui-widget-content' style='width: 110px; height:40px;'>Button</div>";
    
-   div.className = "ui-widget-content";
+   div.class = "ui-widget-content";
    div.id = cVarName;
    div.style.position = "absolute";
    div.style.width  = "110px";
