11d76ec0e2e19862852b9b6e92f14627a143e402
antonio.fivetech@gmail.com <antonio.fivetech@gmail.com@b5e14407-53c1-d272-d7d5-3beee06475fb>
2012-11-21 21:17:54 +0000

    + Modified javascript function AddButton()
    git-svn-id: https://fiveweb.googlecode.com/svn/trunk@146 b5e14407-53c1-d272-d7d5-3beee06475fb

--- source/js/fiveweb.js
+++ source/js/fiveweb.js
@@ -49,9 +49,19 @@ function AddButton( cDlgName )
 {
    var oDlg = document.getElementById( cDlgName );
    var cVarName = "ctrl" + ( oDlg.children.length + 1 );
+   var div = document.createElement( "div" );
    
-   oDlg.innerHTML += "<div id='" + cVarName + "' class='ui-widget-content' style='width: 110px; height:40px;'>Button</div>";
+   // oDlg.innerHTML += "<div id='" + cVarName + "' class='ui-widget-content' style='width: 110px; height:40px;'>Button</div>";
+   
+   div.className = "ui-widget-content";
+   div.id = cVarName;
+   div.style.position = "absolute";
+   div.style.width  = 110px;
+   div.style.height = 40px;
+
    $( "#" + cVarName ).button().resizable().draggable();
+      
+   oDlg.appendChild( div );
 }   
 
 function AddSay( cDlgName )
