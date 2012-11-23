2cdc055a8ba9e7ef33bf905d80c26e7cc0923749
antonio.fivetech@gmail.com <antonio.fivetech@gmail.com@b5e14407-53c1-d272-d7d5-3beee06475fb>
2012-11-23 09:12:07 +0000

    git-svn-id: https://fiveweb.googlecode.com/svn/trunk@211 b5e14407-53c1-d272-d7d5-3beee06475fb

--- source/js/fiveweb.js
+++ source/js/fiveweb.js
@@ -106,7 +106,7 @@ function GenCode( cDlgName )
 {
    var oDlg    = document.getElementById( cDlgName );
    var cCode   = '#include "FiveWeb.ch"' + CRLF + CRLF;
-   var cWidth  = oDlg.style.width.toString();
+   var cWidth  = oDlg.css( "width" ).toString();
    var cHeight = oDlg.style.height.toString();
    
    cWidth  = cWidth.substr( 0, cWidth.length - 2 );
