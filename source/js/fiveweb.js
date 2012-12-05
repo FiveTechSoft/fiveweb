4848db55c025efd35388f9ee3f63cc0331b53113
antonio.fivetech@gmail.com <antonio.fivetech@gmail.com@b5e14407-53c1-d272-d7d5-3beee06475fb>
2012-12-05 12:28:18 +0000

    * More on previous commit
    git-svn-id: https://fiveweb.googlecode.com/svn/trunk@302 b5e14407-53c1-d272-d7d5-3beee06475fb

--- source/js/fiveweb.js
+++ source/js/fiveweb.js
@@ -61,6 +61,7 @@ function CenterHorizontally()
       width1 = $( "#oDlg" ).css( "width" );
       width2 = oCtrl.css( "width" );
       pos = ( width1 / 2 ) - ( width2 / 2 );	
+      MsgInfo( pos );
       oCtrl.css( "left", pos );
    }   
 }      	
