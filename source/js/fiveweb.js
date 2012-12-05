e00f77a12e315c1d50c42469cd211bfb29e5b532
antonio.fivetech@gmail.com <antonio.fivetech@gmail.com@b5e14407-53c1-d272-d7d5-3beee06475fb>
2012-12-05 11:16:27 +0000

    * More on previous commit
    git-svn-id: https://fiveweb.googlecode.com/svn/trunk@278 b5e14407-53c1-d272-d7d5-3beee06475fb

--- source/js/fiveweb.js
+++ source/js/fiveweb.js
@@ -203,6 +203,7 @@ function ShowPopup( cDlgName )
 
    $( "#popup" ).menu();
    $( "#popup" ).css( 'background-image', "none" );
+   $( "#popup" ).bind( "menuselect", function( event, ui ) { $( "#popup" ).destroy(); } );
 }   
 
 function GenCode( cDlgName )
