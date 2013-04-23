04dfd5c72ba4425d7ba2e305fd4ee56f61929f8e
antonio.fivetech@gmail.com <antonio.fivetech@gmail.com@b5e14407-53c1-d272-d7d5-3beee06475fb>
2013-04-23 07:54:58 +0000

    * Minor fix to previous commit
    git-svn-id: https://fiveweb.googlecode.com/svn/trunk@388 b5e14407-53c1-d272-d7d5-3beee06475fb

--- source/js/fiveweb.js
+++ source/js/fiveweb.js
@@ -691,7 +691,7 @@ function Dialog( cTitle, nWidth, nHeight, lModal )
    this.dlg.id = "dialog";
    this.dlg.title = cTitle;
    
-   this.End = function() { MsgInfo( "si" ); $( "#dialog" ).dialog( "destroy" ); };
+   this.End = function() { $( "#dialog" ).dialog( "close" ).remove(); };
 
    document.body.appendChild( this.dlg );
 
